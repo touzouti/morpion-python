4659c4c0251d3cd809b1a95426710cf67b146dd9
Imène <imenetouzouti20@gmail.com>
2026-08-19 09:26:02 +0200

    chore: initialise le projet Tkinter

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+
+def main():
+    fenetre = tk.Tk()
+    fenetre.title("Morpion")
+    fenetre.geometry("400x400")
+    fenetre.mainloop()
+
+
+if __name__ == "__main__":
+    main()
